8a224ad6bc67b31ef69431e683e262597a5425e0
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-12-15 22:13:42 +0600

    Code updated

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,7 +4,7 @@ import User from '../User/User';
 
 const Users = ({ users }) => {
     return (
-        <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]} style={{ width: '100%', margin: '0 auto' }}>
+        <Row >
             {
                 users.map(user => <User
                     key={user.id}
